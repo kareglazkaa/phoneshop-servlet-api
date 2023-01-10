764e90dcba31d374a56193def2fb9fac7c74115f
Acer <sazonova_darya@outlook.com>
2023-01-10 15:24:10 +0300

    Revert "Revert "Task 1.5: Implement ProductDao""
    This reverts commit 1ec1b3a3f3eef04a9970a64570376dc0a7c55a07.

--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.anyString;
