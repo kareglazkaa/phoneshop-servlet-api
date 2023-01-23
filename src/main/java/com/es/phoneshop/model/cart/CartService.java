dec13ded2769b37900b16c65864d66cf26b11d16
Acer <sazonova_darya@outlook.com>
2023-01-23 10:43:54 +0300

    Revert "Revert "Task4""
    This reverts commit 62095ffb829f5c8333c722cb10dc46b541fd5d93.

--- src/main/java/com/es/phoneshop/model/cart/CartService.java
+++ src/main/java/com/es/phoneshop/model/cart/CartService.java
@@ -1,10 +1,14 @@
 package com.es.phoneshop.model.cart;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public interface CartService {
-    Cart getCart(HttpServletRequest request);
+    Cart getCart(HttpSession session);
 
-    void addItem(Cart cart, Long productId, int quantity) throws OutOfStockException;
+    void add(Cart cart, Long productId, int quantity) throws OutOfStockException;
 
+    void update(Cart cart, Long productId, int quantity) throws OutOfStockException;
+
+    void delete(Cart cart, Long productId);
 }
--- src/main/java/com/es/phoneshop/web/DemoDataServletContextListener.java
+++ src/main/java/com/es/phoneshop/web/DemoDataServletContextListener.java
@@ -14,7 +14,7 @@ import java.util.Currency;
 import java.util.List;
 
 public class DemoDataServletContextListener implements ServletContextListener {
-    private ProductDao productDao = ArrayListProductDao.getINSTANCE();
+    private ProductDao productDao = ArrayListProductDao.getInstance();
 
 
     public DemoDataServletContextListener() {
